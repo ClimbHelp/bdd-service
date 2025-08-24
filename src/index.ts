ca1fca7613c10b436b01d77da6b112888d022aea
Alexiiisv <alvelleine@gmail.com>
2025-08-24 18:14:07 +0200

    last miracle

--- src/index.ts
+++ src/index.ts
@@ -94,8 +94,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 module.exports = app;
 
 // Démarrage du serveur seulement en développement
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//   });
+// }
